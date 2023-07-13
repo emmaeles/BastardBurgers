e902457a2acf8c8128fceda451eeeba65fa17d4a
Hjalmar von Wachenfeldt <hjalmarvonwachefeldt@outlook.com>
2023-07-13 13:17:59 +0200

    did some changes

--- bastard/src/App.js
+++ bastard/src/App.js
@@ -2,63 +2,106 @@ import image from './images/Bastardburgers.jpg'
 import imageBurger from './images/burger.jpg'
 import imageSide from './images/Pommes.png'
 import imageDrink from './images/pepsi.png'
+import cheese from './images/cheese.png'
+import meatPatty from './images/meat patty.png'
+import bacon from './images/bacon.png'
+import jalapenos from './images/Jalapenos.png'
+import shackFries from './images/ShackFries.png'
+import sweetPotato from './images/SweetPotatoFries.png'
+import animalStyle from './images/AnimalStyleFries.png'
+import pepsi from './images/litenPepsi.png' 
+import pepsiMax from './images/PepsiMax.png' 
+import sevenUP from './images/7UPFree.png'
+
+
+
+
+
 import { useState} from 'react'
 
 const addProduct = new Map([
-  ['p1', { name: 'Cheese', price: 15 }],
-  ['p2', { name: 'Meat Patty', price: 45 }],
-  ['p3', { name: 'Bacon', price: 20 }]
+  ['p1', { name: 'Cheese', price: 15, image: cheese }],
+  ['p2', { name: 'Meat Patty', price: 45, image: meatPatty }],
+  ['p3', { name: 'Bacon', price: 20, image: bacon }],
+  ['p4', { name: 'Jalapeños', price: 10, image: jalapenos }]
 ])
 const sides = new Map([
-  ['s1', { name: 'Shack Fries', price: 0 }],
-  ['s2', { name: 'Sweet Potato Fries', price: 10 }],
-  ['s3', { name: 'Animal Style Fries', price: 39 }]
+  ['s1', { name: 'Shack Fries', price: 0, image: shackFries}],
+  ['s2', { name: 'Sweet Potato Fries', price: 10, image: sweetPotato}],
+  ['s3', { name: 'Animal Style Fries', price: 39, image: animalStyle }]
 ])
 const drinks = new Map([
-  ['d1', { name: 'Pepsi', price: 0 }],
-  ['d2', { name: 'Pepsi Max', price: 0 }],
-  ['d3', { name: '7up Free', price: 0 }]
-])
+  ['d1', { name: 'Pepsi', price: 0, image: pepsi }],
+  ['d2', { name: 'Pepsi Max', price: 0, image: pepsiMax }],
+  ['d3', { name: '7up Free', price: 0, image: sevenUP }]
+]);
 
 const list =[addProduct, sides, drinks]
 
 
 
 function getProduct(id) {
-  return addProduct.get(id)
+  return addProduct.get(id);
 }
 
-function ProductItem(props) {
-  const keys = Object.keys(addProduct);
-  const addProduct1 = getProduct(keys)
+function ProductItem({ id }) {
+  const product = getProduct(id);
 
   return (
     <div className='d-flex justify-content-between'>
-      {console.log(keys)}
-      {/* {addProduct1.name} +{addProduct1.price} */}
-      <button type="button" class="btn btn-dark btn-add justify-content-between">+</button>      
-    </div> 
-    
-  )
-
+      <div>
+        <img src={product.image}  className="product-image" />
+        <span>{product.name}</span>
+      </div>
+      <div>
+        +{product.price}
+        <button type="button" className="btn btn-dark btn-add justify-content-between">+</button>
+      </div>
+    </div>
+  );
 }
 
-// function getSides(id) {
-//   return sides.get(id)
-// }
+function getSides(id) {
+  return sides.get(id);
+}
 
-// function SidesItem(props) {
-//   const keys = Object.keys(sides);
-//   const sides = getSides(keys)
+function SidesItem({ id }) {
+  const side = getSides(id);
 
-//   return (
-//     <div className='d-flex justify-content-between'>
-//       {sides.name} +{sides.price}
-//       <button type="button" class="btn btn-dark btn-add justify-content-between">+</button>      
-//     </div>
-    
-//   )
-// }
+  return (
+    <div className='d-flex justify-content-between'>
+      <div>
+        <img src={side.image}  className="product-image" />
+        <span>{side.name}</span>
+      </div>
+      <div>
+        +{side.price}
+        <button type="button" className="btn btn-dark btn-add justify-content-between">+</button>
+      </div>
+    </div>
+  );
+}
+
+function getDrinks(id) {
+  return drinks.get(id);
+}
+
+function DrinksItem({ id }) {
+  const drink = getDrinks(id);
+
+  return (
+    <div className='d-flex justify-content-between'>
+      <div>
+        <img src={drink.image} className="product-image" />
+        <span>{drink.name}</span>
+      </div>
+      <div>
+        +{drink.price}
+        <button type="button" className="btn btn-dark btn-add justify-content-between">+</button>
+      </div>
+    </div>
+  );
+}
 
 function App() {
 
@@ -66,12 +109,13 @@ function App() {
     { id: "p1", amount: 0},
     { id: "p2", amount: 0},
     { id: "p3", amount: 0}, 
-    // { id: "s1", amount: 0 },
-    // { id: "s2", amount: 0 },
-    // { id: "s3", amount: 0 },
-    // { id: "d1", amount: 0 },
-    // { id: "d2", amount: 0 },
-    // { id: "d3", amount: 0 },
+    { id: "p4", amount: 0}, 
+    { id: "s1", amount: 0 },
+    { id: "s2", amount: 0 },
+    { id: "s3", amount: 0 },
+    { id: "d1", amount: 0 },
+    { id: "d2", amount: 0 },
+    { id: "d3", amount: 0 },
   ])
 
   return (
@@ -98,16 +142,23 @@ function App() {
         <div className="col">
           <img src={imageBurger} className="bigMenuImages" />
           <h2>Add</h2>
-           {<ProductItem/>}
+          {Array.from(addProduct.keys()).map((key) => (
+          <ProductItem key={key} id={key} />
+        ))}
         </div>
         <div className="col">
           <img src={imageSide} className="bigMenuImages" />
           <h2>Choose your Side</h2>
-          {/* {<SidesItem />} */}
+          {Array.from(sides.keys()).map((key) => (
+          <SidesItem key={key} id={key} />
+        ))}
         </div>
         <div className="col">
           <img src={imageDrink} className="bigMenuImages" />
           <h2>Choose Your Drink</h2>
+          {Array.from(drinks.keys()).map((key) => (
+          <DrinksItem key={key} id={key} />
+        ))}
 
         </div>
         {/* </div> */}
