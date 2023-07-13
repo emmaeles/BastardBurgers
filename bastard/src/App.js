bd682c6adf8bf9950b621689143358b9b80bb151
Emma Eles Bohlin <emma.eles@brightstraining.com>
2023-07-13 09:54:02 +0200

    Added menu

--- bastard/src/App.js
+++ bastard/src/App.js
@@ -1,26 +1,50 @@
 import image from './images/Bastardburgers.jpg'
+import imageBurger from './images/burger.jpg'
+import imageSide from './images/Pommes.png'
+import imageDrink from './images/pepsi.png'
+
 
 function App() {
   return (
     <article>
-      
 
-    <div className="head">
 
-      <div className="caprasimo"><h1>PINEAPPLE EXPRESS MEAL</h1>
+      <div className="head">
 
-      <div className="sidebar"> 
+        <div className="caprasimo"><h1>PINEAPPLE EXPRESS MEAL</h1>
 
-        <img className='img' src={image} />
+          <div className="sidebar">
+
+            <img className='logo' src={image} />
+
+          </div>
+
+
+        </div>
 
       </div>
 
-      </div>
+      <div className="container text-center justify-content-evenly menu">
+        <div class="row">
+          <div className="col">
+            <img src={imageBurger} className="bigMenuImages" />
+            <h2>Add</h2>
+          </div>
+          <div className="col">
+            <img src={imageSide} className="bigMenuImages" />
+            <h2>Choose your Side</h2>
 
-    </div>
+          </div>
+          <div className="col">
+            <img src={imageDrink} className="bigMenuImages" />
+            <h2>Choose Your Drink</h2>
+
+          </div>
+        </div>
+      </div>
 
     </article>
-    
+
   );
 }
 
